11819aba57afc3396144b5c8420ac35ccbb1743b
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-03 09:46:07 +0000

    Implemented generate maze package

--- src/GameMap.js
+++ src/GameMap.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { roomInfo, roomSize } from './roomGenerator';
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import generateMaze from 'generate-maze';
+
+const DrawMap = styled.div`
+  line-height: 0;
+`;
 
 const DrawRoom = styled.div`
   display: inline-block;
   width: 50px;
   height: 50px;
-  background: rgb(30,30,30);
-  margin-left: 1rem;
+  background: rgb(230,230,230);
 `
 const InfoRoom = styled.div`
   display: block;
@@ -19,24 +24,21 @@ const InfoRoom = styled.div`
 `
 
 function GameMap() {
-  const [rooms, setRooms] = useState([])
+  const maze = generateMaze(20);
+  console.log(maze);
 
-  useEffect(() => {
-    for (let i = 0; i < 100; i++) {
-      setRooms(rooms => [...rooms, {
-        title: "random",
-        description: roomInfo(),
-        size: roomSize(),
-      }])
-    }
-  }, [])
-
-  if (rooms.length > 0) {
-    return <div id="map">
+  if (maze.length > 0) {
+    return <DrawMap>
       {
-        rooms.map((room, i) => <Room key={i} draw {...room} />)
+        maze.map(
+          (row, r) =>
+            row.map(
+              (cell, c) =>
+                <Room key={`${r}-${c}`} draw {...cell} />
+            )
+        )
       }
-    </div>
+    </DrawMap>
   } else {
     return <p>Loading...</p>
   }
@@ -44,11 +46,23 @@ function GameMap() {
 
 function Room(props) {
   if (props.draw) {
-    return <DrawRoom style={{ width: props.size.width + 'px', height: props.size.height + 'px' }}></DrawRoom>
+    let marginTop = 0
+    let marginLeft = 0
+    if (props.top === true) marginTop--;
+    if (props.bottom === true) marginTop--;
+    if (props.left === true) marginLeft--;
+    if (props.right === true) marginLeft--;
+    return <DrawRoom style={{
+      borderTop: (props.top === true ? '1px solid red' : null),
+      borderBottom: (props.bottom === true ? '1px solid red' : null),
+      borderLeft: (props.left === true ? '1px solid red' : null),
+      borderRight: (props.right === true ? '1px solid red' : null),
+      marginTop: marginTop + 'px',
+      marginLeft: marginLeft + 'px'
+    }} />
   } else {
     return <InfoRoom>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
+      <p></p>
     </InfoRoom>
   }
 }
