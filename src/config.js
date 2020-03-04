4519cc658aaf39f8f395b207f4387bceaf58b435
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-04 19:24:41 +0000

    Added pusher implementation

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const userTokenKey = "mud_token";
 export const baseUrl = "https://mud-api-20.herokuapp.com";
+export const pusherKey = "6c3071d610bd137e36cd";
--- src/game/GameMap.js
+++ src/game/GameMap.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Pusher from 'pusher-js';
+import { pusherKey } from '../config';
 import Styl from "../styledComponents";
 import axios from '../hooks/useAxios';
 import useRooms from '../context/rooms';
@@ -36,10 +37,11 @@ function GameMap(props) {
   const [initState, initActions] = useInit();
   const [roomState, roomActions] = useRooms();
   const [playerTravel, setPlayerTravel] = useState('');
+  const [message, setMessage] = useState('')
   const { gameMap, rooms } = roomState;
 
+  // allow player movement with keyboard arrows
   useEffect(() => {
-
     document.addEventListener('keydown', function (e) {
       const key = e.which;
       if ([37, 38, 39, 40].includes(key)) e.preventDefault();
@@ -65,15 +67,27 @@ function GameMap(props) {
       }
     })
   });
-
+  // initialize player data
   useEffect(() => {
-    if (!initState.init.uuid) initActions.getInit();
+    if (!initState.init.uuid) {
+      initActions.getInit();
+    } else {
+      const pusher = new Pusher(pusherKey, {
+        cluster: 'us3',
+        encrypted: true
+      });
+      const channel = pusher.subscribe(`p-channel-${initState.init.uuid}`);
+      channel.bind('broadcast', data => {
+        console.log('broadcast data', data)
+        setMessage(data.message);
+      });
+    }
   }, [initState, initActions]);
-
+  // get the map with all rooms
   useEffect(() => {
     if (!roomState.rooms.length > 0) roomActions.getRooms();
   }, [roomState, roomActions]);
-
+  // request player movement
   useEffect(() => {
     if (playerTravel !== '') {
       console.log('playerTravel', playerTravel)
@@ -83,26 +97,6 @@ function GameMap(props) {
       })
     }
   }, [playerTravel])
-
-  function Message() {
-  
-    const [message, setMessage] = useState('')
-  
-    useEffect(() => {
-      const pusher = new Pusher('6c3071d610bd137e36cd', {
-        cluster: 'us3',
-        encrypted: true
-      });
-      const channel = pusher.subscribe('p-channel-c76cb378-9bd0-4614-a632-6be5c51c7276');
-      channel.bind('broadcast', data => {
-        console.log('data', data)
-        setMessage(data.message);
-      });
-    })
-    return (
-      <div>{message}</div>
-    )
-  }
 
   const changePlayerDirection = (e) => {
     const { value } = e.target;
@@ -137,16 +131,7 @@ function GameMap(props) {
           <div className="column">
             <Styl.Title className="small">Room Chat</Styl.Title>
             <Styl.Chat>
-              <p>Someone left the room.</p>
-              <p>Someone entered the room.</p>
-              <p>Someone entered the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
-              <p>Someone left the room.</p>
+              {message}
             </Styl.Chat>
           </div>
           <div className="column">
