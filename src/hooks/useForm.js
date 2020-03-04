d87c088288171c774f6de126df735437434f50f2
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-04 19:13:56 +0000

    Fixed uncontrolled inputs error in forms

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 const useForm = callback => {
-  const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+    password1: "",
+    password2: "",
+    email: "",
+  });
 
   const handleSubmit = event => {
     if (event) {
