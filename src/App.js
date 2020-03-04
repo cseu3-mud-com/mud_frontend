ca0612b764a360c9b8653c8c648e904ff175c91c
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-04 14:47:55 +0000

    Added styled components with theme provider to main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import './App.css';
 import GameCharCreator from './character/GameCharCreator';
 import GameMenu from './game/GameMenu';
-import GameMap from './game/GameMap';
 import GameMapServer from './game/GameMapServer';
-import Landing from "./auth/Landing";
 import SignupForm from "./auth/SignupForm";
 import LoginForm from "./auth/LoginForm";
 import PrivateRoute from "./hooks/usePrivateRoute";
@@ -14,19 +11,38 @@ import {
   Route
 } from "react-router-dom";
 
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './globalStyles';
+import Styl from './styledComponents';
+
+const theme = {
+  fonts: {
+    primary: "'Roboto Mono', monospace, sans-serif",
+    secondary: "'Trade Winds', cursive, sans-serif",
+  },
+  colors: {
+    white: '#fff'
+  },
+  pageBackground: '#000'
+}
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/signup" component={SignupForm} />
-        <Route path="/login" component={LoginForm} />
-        <PrivateRoute path="/gamemenu" component={GameMenu} />
-        <PrivateRoute path="/character" component={GameCharCreator} />
-        <PrivateRoute path="/map" component={GameMap} />
-        <PrivateRoute path="/mapServer" component={GameMapServer} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Styl.Page>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LoginForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={SignupForm} />
+            <PrivateRoute path="/menu" component={GameMenu} />
+            <PrivateRoute path="/map" component={GameMapServer} />
+            <PrivateRoute path="/character" component={GameCharCreator} />
+          </Switch>
+        </Router>
+      </Styl.Page>
+    </ThemeProvider>
   );
 }
 
