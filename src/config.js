8bfd4ee5b7b6484b0d057d35f1a5b929820402a9
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-05 09:13:43 +0000

    Added deployment pusher key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const userTokenKey = "mud_token";
-export const baseUrl = "http://127.0.0.1:8000"; //"https://mud-api-20.herokuapp.com";
-export const pusherKey = "6c3071d610bd137e36cd";
+export const baseUrl = "https://mud-api-20.herokuapp.com"; // "http://127.0.0.1:8000";
+export const pusherKey = "dd78c8bdfd2684c4767e"; // "6c3071d610bd137e36cd"
